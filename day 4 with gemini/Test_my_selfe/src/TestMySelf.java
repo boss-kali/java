0f49a81745abc6414abb315a78a60cbe17bac972
boss-kali <gamingroodetc@gmail.com>
2026-02-01 16:54:06 +0300

    add

--- day 4 with gemini/Test_my_selfe/src/TestMySelf.java
+++ day 4 with gemini/Test_my_selfe/src/TestMySelf.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
+
 public class TestMySelf {
     static Scanner input = new Scanner(System.in);
-    public static void main(String[] args)  {
+
+    public static void main(String[] args) {
 
         System.out.println(" ========================================== ");
         System.out.println("little porject to add employees and managers");
@@ -9,8 +11,8 @@ public class TestMySelf {
         System.out.print("How many names you want to enter : ");
         int size = input.nextInt();
         Employee[] team = new Employee[size];
-        for (int i = 0 ; i < size; i++) {
-            System.out.println("\n --- person " + (i+1) + " ---");
+        for (int i = 0; i < size; i++) {
+            System.out.println("\n --- person " + (i + 1) + " ---");
             System.out.print("Type ? ( 1- for Employee - 2- for manager :");
             int type = getValidInt();
             input.nextLine();
@@ -22,35 +24,35 @@ public class TestMySelf {
                 System.out.print("Bonus : ");
                 double bonus = getValidDouble();
                 team[i] = new Manager(name, salary, bonus);
-            }else{
-                team[i] = new Employee(name , salary);
+            } else {
+                team[i] = new Employee(name, salary);
             }
         }
         System.out.println(" Company team ");
-        for(int i = 0; i < team.length; i++) {
+        for (int i = 0; i < team.length; i++) {
             team[i].printInfo();
-            if( team[i] instanceof Manager) {
+            if (team[i] instanceof Manager) {
                 ((Manager) team[i]).signPaper();
             }
         }
     }
+
     public static double getValidDouble() {
-        while (true){
-            try{
+        while (true) {
+            try {
                 return input.nextDouble();
-            }
-            catch (Exception e) {
+            } catch (Exception e) {
                 System.out.println(" !! enter just numbers");
                 input.next();
             }
         }
     }
+
     public static int getValidInt() {
-        while(true){
+        while (true) {
             try {
                 return input.nextInt();
-            }
-            catch(Exception e) {
+            } catch (Exception e) {
                 System.out.println(" !! enter just numbers");
                 input.next();
             }
