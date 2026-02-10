b37d039624f62a15a85aab72f33b061908a47a4d
boss-kali <gamingroodetc@gmail.com>
2026-02-10 22:35:47 +0300

    PrintF

--- learn printF/src/LearnPrintF.java
+++ learn printF/src/LearnPrintF.java
@@ -9,5 +9,9 @@ public class LearnPrintF {
         String product = "Laptop";
         double price = 1250.5;
         System.out.printf("Product: %s | price: %.2f", product, price);
+        // %-10s : يعني احجز 10 خانات للاسم و السالب يعني ابدا كتابه من اليسار
+        // رقم 10 مثل عرض الحقل
+        // % ,.2f : الفاصلة هنا للتفريق بين الالاف
+        System.out.printf("\nProduct : %-10s || Prince: $%,.2f %n", product, price);
     }
 }
