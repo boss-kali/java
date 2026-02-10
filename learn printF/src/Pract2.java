a24a5768c196f61d5d74daa10200c318cb8bbab1
boss-kali <gamingroodetc@gmail.com>
2026-02-11 01:43:41 +0300

    Pract 2

---
+++ learn printF/src/Pract2.java
@@ -0,0 +1,9 @@
+public class Pract2 {
+    public static void main(String[] args) {
+        int number = 8;
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf(" %d * %2d =  %2d %n", number, i, i * number);
+        }
+    }
+
+}
